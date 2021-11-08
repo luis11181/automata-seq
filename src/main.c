c0553067511de7ad581220d32e3fb604a70794bc
luis11181 <luis11181@hotmail.com>
2021-11-07 20:03:46 -0500

    message element

--- src/main.c
+++ src/main.c
@@ -144,43 +144,7 @@ int main(int argc, char **argv)
     SDL_Event event;
     bool draw;
     int drawing_element = FIRE;
-
-    //this opens a font style and sets a size
-TTF_Font* Sans = TTF_OpenFont("Sans.ttf", 24);
-
-// this is the color in rgb format,
-// maxing out all would give you the color white,
-// and it will be your text's color
-SDL_Color White = {255, 255, 255};
-
-// as TTF_RenderText_Solid could only be used on
-// SDL_Surface then you have to create the surface first
-SDL_Surface* surfaceMessage =
-    TTF_RenderText_Solid(Sans, "put your text here", White); 
-
-// now you can convert it into a texture
-SDL_Texture* Message = SDL_CreateTextureFromSurface(renderer, surfaceMessage);
-
-SDL_Rect Message_rect; //create a rect
-Message_rect.x = 0;  //controls the rect's x coordinate 
-Message_rect.y = 0; // controls the rect's y coordinte
-Message_rect.w = 100; // controls the width of the rect
-Message_rect.h = 100; // controls the height of the rect
-
-// (0,0) is on the top left of the window/screen,
-// think a rect as the text's box,
-// that way it would be very simple to understand
-
-// Now since it's a texture, you have to put RenderCopy
-// in your game loop area, the area where the whole code executes
-
-// you put the renderer's name first, the Message,
-// the crop size (you can ignore this if you don't want
-// to dabble with cropping), and the rect which is the size
-// and coordinate of your texture
-SDL_RenderCopy(renderer, Message, NULL, &Message_rect);
-
-//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+       
 
     while (state.mode != QUIT_MODE) {
       //! while loop to search for events and handle them doing an action dependong on the game
@@ -253,6 +217,21 @@ SDL_RenderCopy(renderer, Message, NULL, &Message_rect);
                     }           
                     else if (event.key.keysym.sym == 'f' || event.key.keysym.sym == 'F') {
                       drawing_element = FIRE;
+                      
+                      TTF_Font* Sans =TTF_OpenFont("Sans.ttf", 20);
+                      SDL_Color white_font = { .r = 0, .g = 0, .b = 0 };
+                      //render text on screen with SDL with the element that is being drawn 
+                      SDL_Surface *surfaceMessage = TTF_RenderText_Solid(Sans, "FIRE", white_font); //
+                      SDL_Texture *Message = SDL_CreateTextureFromSurface(renderer, surfaceMessage); // now you can convert it into a texture
+                      SDL_Rect Message_rect;
+                      Message_rect.x = 0;
+                      Message_rect.y = 0;
+                      Message_rect.w = surfaceMessage->w;
+                      Message_rect.h = surfaceMessage->h;
+                      SDL_RenderCopy(renderer, Message, NULL, &Message_rect);
+                      SDL_FreeSurface(surfaceMessage);
+                      SDL_DestroyTexture(Message);
+                      
                     }                                       
                     else if (event.key.keysym.sym == 's' || event.key.keysym.sym == 'S') {
                       drawing_element = SAND;
